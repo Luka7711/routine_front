527cdb76fa0fa1743c252e7229517543ba182c7a
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2019-09-05 11:49:35 -0500

    sending user data to database to register

--- src/Signup/index.js
+++ src/Signup/index.js
@@ -6,7 +6,8 @@ class Signup extends Component {
 		this.state = {
 			username:'',
 			password:'',
-			zodiac:''
+			zodiac:'',
+			message:''
 		}
 	}
 
@@ -14,6 +15,41 @@ handleChange = (e) =>{
 	this.setState({
 		[e.currentTarget.name]: e.currentTarget.value
 	})
+}
+
+handleSubmit = async(e) => {
+	e.preventDefault();
+	console.log(this.state)
+	
+	try{
+		const response = await fetch('http://localhost:9000/auth/register', {
+			method:'POST',
+			credentials:'include',
+			body:JSON.stringify(this.state),
+			headers:{
+				'Content-Type':'application/json'
+			}
+		})
+
+		const parsedResponse = await response.json();
+
+		if(parsedResponse.status === 200){
+			this.setState({
+				message:'Thank you for registerring'
+			})
+			console.log(this.state.message)
+		}else{
+			this.setState({
+				message:'Not able to sign you up'
+			})
+			console.log(this.state.message)
+		}
+	}catch(err){
+		this.setState({
+			message:'Something went wrong, try again!'
+		})
+		console.log(this.state.message)
+	}
 }
 	render(){
 		return(
@@ -26,7 +62,7 @@ handleChange = (e) =>{
 						<label>password:</label>
 						<input placeholder="password" name="password" onChange={this.handleChange}/>
 						<label>zodiac:</label>
-						<select onChange={this.handleChange}>
+						<select onChange={this.handleChange} name="zodiac">
 							<option value="aries">aries</option>
 							<option value="taurus">taurus</option>
 							<option value="gemini">gemini</option>
